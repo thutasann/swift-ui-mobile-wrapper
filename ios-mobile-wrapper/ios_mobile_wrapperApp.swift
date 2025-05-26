4ceb512f91733c1ef9549c0da468eb8ef0ca8307
thutasann <thutasann2002@gmail.com>
2025-05-26 22:45:04 +0800

    Initial Commit

---
+++ ios-mobile-wrapper/ios_mobile_wrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_mobile_wrapperApp.swift
+//  ios-mobile-wrapper
+//
+//  Created by Thuta sann on 5/26/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_mobile_wrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
